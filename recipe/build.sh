ea12f03a48c4b3763848ae17e5a9c49a587fa970
Serhii Kupriienko <skupriienko@anaconda.com>
2025-02-07 18:42:10 +0200

    20240312

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
-set -e
-set -x
+set -ex
 
-# From: https://github.com/TeX-Live/texlive-source/blob/trunk/Build
+export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+export LIBRARY_PATH="$LIBRARY_PATH:$SDKROOT/usr/lib"
+
 unset TEXMFCNF; export TEXMFCNF
 LANG=C; export LANG
-# [[ -d "${PREFIX}"/texmf ]] || mkdir -p "${PREFIX}"/texmf
-# ./configure --help
+
+# Need the fallback path for testing in some cases.
+if [ "$(uname)" == "Darwin" ]
+then
+    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+else
+    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
 
 # Using texlive just does not work, various sub-parts ignore that and use PREFIX/share
 # SHARE_DIR=${PREFIX}/share/texlive
@@ -16,12 +23,12 @@ SHARE_DIR=${PREFIX}/share
 declare -a CONFIG_EXTRA
 if [[ ${target_platform} =~ .*ppc.* ]]; then
   # luajit is incompatible with powerpc.
-  CONFIG_EXTRA+=(-disable-luajittex)
+  CONFIG_EXTRA+=(--disable-luajittex)
+  CONFIG_EXTRA+=(--disable-mfluajit)
 fi
 
-TEST_SEGFAULT=yes
+TEST_SEGFAULT=no
 
-# if [[ ${TEST_SEGFAULT} == yes ]] && [[ ${target_platform} =~ .*linux.* ]]; then
 if [[ ${TEST_SEGFAULT} == yes ]]; then
   # -O2 results in:
   # FAIL: mplibdir/mptraptest.test
@@ -34,34 +41,35 @@ else
   CONFIG_EXTRA+=(--disable-debug)
 fi
 
-
 # kpathsea scans the texmf.cnf file to set up its hardcoded paths, so set them
 # up before building. It doesn't seem to handle multivalued TEXMFCNF entries,
 # so we patch that up after install.
-# Requires prefix replacement, which does not work correctly.
-mv "${SRC_DIR}"/texk/kpathsea/texmf.cnf tmp.cnf
+
+mv $SRC_DIR/texk/kpathsea/texmf.cnf tmp.cnf
 sed \
     -e "s|TEXMFROOT =.*|TEXMFROOT = ${SHARE_DIR}|" \
     -e "s|TEXMFLOCAL =.*|TEXMFLOCAL = ${SHARE_DIR}/texmf-local|" \
     -e "/^TEXMFCNF/,/^}/d" \
     -e "s|%TEXMFCNF =.*|TEXMFCNF = ${SHARE_DIR}/texmf-dist/web2c|" \
-    <tmp.cnf >"${SRC_DIR}"/texk/kpathsea/texmf.cnf
+    <tmp.cnf >$SRC_DIR/texk/kpathsea/texmf.cnf
 rm -f tmp.cnf
+
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
 
 [[ -d "${SHARE_DIR}/tlpkg/TeXLive" ]] || mkdir -p "${SHARE_DIR}/tlpkg/TeXLive"
 [[ -d "${SHARE_DIR}/texmf-dist/scripts/texlive" ]] || mkdir -p "${SHARE_DIR}/texmf-dist/scripts/texlive"
 
-# Completely essential, see https://github.com/conda-forge/texlive-core-feedstock/issues/19
-find . -name "TexLive"
-install -v -m644 texk/tests/TeXLive/* "${SHARE_DIR}/tlpkg/TeXLive" || exit 1
-install -v -m644 texmf/texmf-dist/scripts/texlive/mktexlsr.pl "${SHARE_DIR}/texmf-dist/scripts/texlive" || exit 1
+# The Makefile also expects OBJCXX to be set. This is vital
+# for cross-compiling, but we should be using the correct compiler
+# aliases everywhere.
+export OBJCXX=$CXX
+export BUILDCC=$CC_FOR_BUILD
 
-export KPATHSEA_WARNING=0
+# We need to package graphite2 to be able to use it harfbuzz.
+# Using our cairo breaks the recipe and `mpfr` is not found triggering the library from TL tree.
 
-mkdir tmp_build || true
-pushd tmp_build
-  ${SRC_DIR}/configure \
-               --prefix="${PREFIX}" \
+mkdir -p tmp_build && pushd tmp_build
+  ../configure --prefix=$PREFIX \
                --host=${HOST} \
                --build=${BUILD} \
                --datarootdir="${SHARE_DIR}" \
@@ -104,49 +112,37 @@ pushd tmp_build
   # There is a race-condition in the build system.
   make -j${CPU_COUNT} ${VERBOSE_AT} || make -j1 ${VERBOSE_AT}
   # make check reads files from the installation prefix:
-  make install-strip -j${CPU_COUNT}
-  make texlinks
-
-  # At this point BLFS does:
-  # tar -xf ../../texlive-20180414-texmf.tar.xz -C /opt/texlive/2018 --strip-components=1
-  # .. but we would like to avoid this 2.5GB of stuff.
-  [[ -d "${SHARE_DIR}/texmf-dist" ]] || mkdir -p "${SHARE_DIR}/texmf-dist"
-  cp -rf "${SRC_DIR}"/texmf/texmf-dist/* "${SHARE_DIR}/texmf-dist/"
-
-  mktexlsr || exit 1
-  fmtutil-sys --all || exit 1
-  mtxrun --generate || exit 1
-
-  #if [[ ! ${target_platform} =~ .*linux.* ]]; then
-  #  LC_ALL=C make check ${VERBOSE_AT}
-  #el
-  if [[ ${TEST_SEGFAULT} == yes ]]; then # && [[ ${target_platform} =~ .*linux.* ]]; then
-    LC_ALL=C make check ${VERBOSE_AT}
-    echo "pushd ${SRC_DIR}/build-tmp/texk/web2c"
-    echo "LC_ALL=C make check ${VERBOSE_AT}"
-    echo "cat mplibdir/mptraptest.log"
-    pushd "${SRC_DIR}/build-tmp/texk/web2c/mpost"
-      # I believe mpost test fails here because it tries to load mpost itself as a configuration file
-      # .. this happens in both failing tests on Linux. Debug builds (CFLAGS-wise) do not suffer a
-      # segfault at this point but release ones. Skipping for now, will re-visit later.
-      LC_ALL=C ../mpost --ini ../mpost
-    popd
-    exit 1
+  make install -j${CPU_COUNT}
+  # Only do make check tests on native builds.
+  if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
+    if [[ ! ${target_platform} =~ .*linux.* ]]; then
+      VERBOSE=1 LC_ALL=C make check ${VERBOSE_AT}
+    elif [[ ${TEST_SEGFAULT} == yes ]] && [[ ${target_platform} =~ .*linux.* ]]; then
+      LC_ALL=C make check ${VERBOSE_AT}
+      echo "pushd ${SRC_DIR}/tmp_build/texk/web2c"
+      echo "LC_ALL=C make check ${VERBOSE_AT}"
+      echo "cat mplibdir/mptraptest.log"
+      pushd "${SRC_DIR}/tmp_build/texk/web2c/mpost"
+        # I believe mpost test fails here because it tries to load mpost itself as a configuration file
+        # .. this happens in both failing tests on Linux. Debug builds (CFLAGS-wise) do not suffer a
+        # segfault at this point but release ones. Skipping for now, will re-visit later.
+        LC_ALL=C ../mpost --ini ../mpost
+      popd
+      exit 1
+    fi
   fi
 popd
 
 # Remove info and man pages.
-rm -rf "${SHARE_DIR}/man"
-rm -rf "${SHARE_DIR}/info"
+rm -rf ${SHARE_DIR}/man
+rm -rf ${SHARE_DIR}/info
 
-mv "${SHARE_DIR}/texmf-dist/web2c/texmf.cnf tmp.cnf"
+mv ${SHARE_DIR}/texmf-dist/web2c/texmf.cnf tmp.cnf
 sed \
     -e "s|TEXMFCNF =.*|TEXMFCNF = {${SHARE_DIR}/texmf-local/web2c, ${SHARE_DIR}/texmf-dist/web2c}|" \
     <tmp.cnf >${SHARE_DIR}/texmf-dist/web2c/texmf.cnf
 rm -f tmp.cnf
 
 # Create symlinks for pdflatex and latex
-pushd "${PREFIX}"/bin
-  ln -s pdftex pdflatex
-  ln -s pdftex latex
-popd
+ln -s $PREFIX/bin/pdftex $PREFIX/bin/pdflatex
+ln -s $PREFIX/bin/pdftex $PREFIX/bin/latex
