ce9ae0268b3b326fc31e92abc6fb9ad205b1d204
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2025-02-11 08:31:19 +0000

    texlive-core 20240312 (#2)
    * 20240312
    
    * Fix pkgconfig path
    
    * Add xproto
    
    * Add libx11
    
    * Add ar on aarch64, skip on s390x
    
    * Clean up the recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,28 +1,42 @@
 #! /bin/bash
 
-set -e
-set -x
+set -ex
 
 # From: https://github.com/TeX-Live/texlive-source/blob/trunk/Build
 unset TEXMFCNF; export TEXMFCNF
 LANG=C; export LANG
-# [[ -d "${PREFIX}"/texmf ]] || mkdir -p "${PREFIX}"/texmf
-# ./configure --help
+
+# Need the fallback path for testing in some cases.
+if [ "$(uname)" == "Darwin" ]
+then
+    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+else
+    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
 
 # Using texlive just does not work, various sub-parts ignore that and use PREFIX/share
 # SHARE_DIR=${PREFIX}/share/texlive
-SHARE_DIR=${PREFIX}/share
+SHARE_DIR="${PREFIX}"/share
 
 declare -a CONFIG_EXTRA
+
 if [[ ${target_platform} =~ .*ppc.* ]]; then
   # luajit is incompatible with powerpc.
-  CONFIG_EXTRA+=(-disable-luajittex)
+  CONFIG_EXTRA+=(--disable-luajittex)
+  CONFIG_EXTRA+=(--disable-mfluajit)
 fi
 
-TEST_SEGFAULT=yes
+# For some weird reason, ar is not picked up on linux-aarch64
+if [ $(uname -s) = "Linux" ] && [ ! -f "${BUILD_PREFIX}/bin/ar" ]; then
+    ln -s "${BUILD}-ar" "${BUILD_PREFIX}/bin/ar"
+    ln -s "$RANLIB" "${BUILD_PREFIX}/bin/ranlib"
+    ln -sf "$LD" "${BUILD_PREFIX}/bin/ld"
+fi
 
-# if [[ ${TEST_SEGFAULT} == yes ]] && [[ ${target_platform} =~ .*linux.* ]]; then
-if [[ ${TEST_SEGFAULT} == yes ]]; then
+# 2025/02/10: tests mplibdir/mptraptest.test & pdftexdir/pdftosrc.test still fail.
+TEST_SEGFAULT=no
+
+if [[ ${TEST_SEGFAULT} == yes ]] && [[ ${target_platform} =~ .*linux.* ]]; then
   # -O2 results in:
   # FAIL: mplibdir/mptraptest.test
   # FAIL: pdftexdir/pdftosrc.test
@@ -33,7 +47,6 @@ if [[ ${TEST_SEGFAULT} == yes ]]; then
 else
   CONFIG_EXTRA+=(--disable-debug)
 fi
-
 
 # kpathsea scans the texmf.cnf file to set up its hardcoded paths, so set them
 # up before building. It doesn't seem to handle multivalued TEXMFCNF entries,
@@ -48,20 +61,29 @@ sed \
     <tmp.cnf >"${SRC_DIR}"/texk/kpathsea/texmf.cnf
 rm -f tmp.cnf
 
+# Needed to find .pc files from CDTs.
+# Tell pkg-config to search the conda-provided sysroot for necessary X11
+# configuration (.pc) files.
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig
+
+# Check cairo cflags. It fails if required CDTs are not present
+echo "Check pkg-config cairo cflags..."
+pkg-config --cflags-only-I cairo
+
 [[ -d "${SHARE_DIR}/tlpkg/TeXLive" ]] || mkdir -p "${SHARE_DIR}/tlpkg/TeXLive"
 [[ -d "${SHARE_DIR}/texmf-dist/scripts/texlive" ]] || mkdir -p "${SHARE_DIR}/texmf-dist/scripts/texlive"
 
-# Completely essential, see https://github.com/conda-forge/texlive-core-feedstock/issues/19
-find . -name "TexLive"
-install -v -m644 texk/tests/TeXLive/* "${SHARE_DIR}/tlpkg/TeXLive" || exit 1
-install -v -m644 texmf/texmf-dist/scripts/texlive/mktexlsr.pl "${SHARE_DIR}/texmf-dist/scripts/texlive" || exit 1
+# The Makefile also expects OBJCXX to be set. This is vital
+# for cross-compiling, but we should be using the correct compiler
+# aliases everywhere.
+export OBJCXX=$CXX
+export BUILDCC=$CC_FOR_BUILD
 
-export KPATHSEA_WARNING=0
+# We need to package graphite2 to be able to use it harfbuzz.
+# Using our cairo breaks the recipe and `mpfr` is not found triggering the library from TL tree.
 
-mkdir tmp_build || true
-pushd tmp_build
-  ${SRC_DIR}/configure \
-               --prefix="${PREFIX}" \
+mkdir -p tmp_build && pushd tmp_build
+  ../configure --prefix=$PREFIX \
                --host=${HOST} \
                --build=${BUILD} \
                --datarootdir="${SHARE_DIR}" \
@@ -104,34 +126,24 @@ pushd tmp_build
   # There is a race-condition in the build system.
   make -j${CPU_COUNT} ${VERBOSE_AT} || make -j1 ${VERBOSE_AT}
   # make check reads files from the installation prefix:
-  make install-strip -j${CPU_COUNT}
-  make texlinks
-
-  # At this point BLFS does:
-  # tar -xf ../../texlive-20180414-texmf.tar.xz -C /opt/texlive/2018 --strip-components=1
-  # .. but we would like to avoid this 2.5GB of stuff.
-  [[ -d "${SHARE_DIR}/texmf-dist" ]] || mkdir -p "${SHARE_DIR}/texmf-dist"
-  cp -rf "${SRC_DIR}"/texmf/texmf-dist/* "${SHARE_DIR}/texmf-dist/"
-
-  mktexlsr || exit 1
-  fmtutil-sys --all || exit 1
-  mtxrun --generate || exit 1
-
-  #if [[ ! ${target_platform} =~ .*linux.* ]]; then
-  #  LC_ALL=C make check ${VERBOSE_AT}
-  #el
-  if [[ ${TEST_SEGFAULT} == yes ]]; then # && [[ ${target_platform} =~ .*linux.* ]]; then
-    LC_ALL=C make check ${VERBOSE_AT}
-    echo "pushd ${SRC_DIR}/build-tmp/texk/web2c"
-    echo "LC_ALL=C make check ${VERBOSE_AT}"
-    echo "cat mplibdir/mptraptest.log"
-    pushd "${SRC_DIR}/build-tmp/texk/web2c/mpost"
-      # I believe mpost test fails here because it tries to load mpost itself as a configuration file
-      # .. this happens in both failing tests on Linux. Debug builds (CFLAGS-wise) do not suffer a
-      # segfault at this point but release ones. Skipping for now, will re-visit later.
-      LC_ALL=C ../mpost --ini ../mpost
-    popd
-    exit 1
+  make install -j${CPU_COUNT}
+  # Only do make check tests on native builds.
+  if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
+    if [[ ! ${target_platform} =~ .*linux.* ]]; then
+      VERBOSE=1 LC_ALL=C make check ${VERBOSE_AT}
+    elif [[ ${TEST_SEGFAULT} == yes ]] && [[ ${target_platform} =~ .*linux.* ]]; then
+      LC_ALL=C make check ${VERBOSE_AT}
+      echo "pushd ${SRC_DIR}/tmp_build/texk/web2c"
+      echo "LC_ALL=C make check ${VERBOSE_AT}"
+      echo "cat mplibdir/mptraptest.log"
+      pushd "${SRC_DIR}/tmp_build/texk/web2c/mpost"
+        # I believe mpost test fails here because it tries to load mpost itself as a configuration file
+        # .. this happens in both failing tests on Linux. Debug builds (CFLAGS-wise) do not suffer a
+        # segfault at this point but release ones. Skipping for now, will re-visit later.
+        LC_ALL=C ../mpost --ini ../mpost
+      popd
+      exit 1
+    fi
   fi
 popd
 
@@ -139,14 +151,12 @@ popd
 rm -rf "${SHARE_DIR}/man"
 rm -rf "${SHARE_DIR}/info"
 
-mv "${SHARE_DIR}/texmf-dist/web2c/texmf.cnf tmp.cnf"
+mv "${SHARE_DIR}"/texmf-dist/web2c/texmf.cnf tmp.cnf
 sed \
     -e "s|TEXMFCNF =.*|TEXMFCNF = {${SHARE_DIR}/texmf-local/web2c, ${SHARE_DIR}/texmf-dist/web2c}|" \
-    <tmp.cnf >${SHARE_DIR}/texmf-dist/web2c/texmf.cnf
+    <tmp.cnf >"${SHARE_DIR}"/texmf-dist/web2c/texmf.cnf
 rm -f tmp.cnf
 
 # Create symlinks for pdflatex and latex
-pushd "${PREFIX}"/bin
-  ln -s pdftex pdflatex
-  ln -s pdftex latex
-popd
+ln -s $PREFIX/bin/pdftex $PREFIX/bin/pdflatex
+ln -s $PREFIX/bin/pdftex $PREFIX/bin/latex
